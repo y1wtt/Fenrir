f24ed06f3fc389c0af6d50dbf6cf83d732fac7a8
awataki <awataki1206@gmail.com>
2020-05-13 00:44:04 +0900

    fix dummy page

--- pages/timer.vue
+++ pages/timer.vue
@@ -1,7 +1,5 @@
 <template>
   <v-container fluid>
-    <v-row align="center" justify="center">
-      <v-col cols="1" sm="1" md="1">次期実装予定</v-col>
-    </v-row>
+    <v-row align="center" justify="center">次期実装予定</v-row>
   </v-container>
 </template>
