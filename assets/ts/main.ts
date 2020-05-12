c40339faa7bb827fabb22c4a879c38e8264ccb15
awataki <awataki1206@gmail.com>
2020-05-12 15:21:54 +0900

    initial commit

---
+++ assets/ts/main.ts
@@ -0,0 +1,21 @@
+export default {
+    data() {
+        return {
+            drawer: false,
+            items: [
+                {
+                    title: "割当ツール",
+                    url: "/position"
+                },
+                {
+                    title: "タイマー",
+                    url: "timer"
+                },
+                {
+                    title: "投票",
+                    url: "/vote"
+                }
+            ]
+        };
+    }
+};
---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <div>
+      <v-navigation-drawer app v-model="drawer" color="blue-grey lighten-5" bottom="true" clipped>
+        <v-list>
+          <v-list-item v-for="i in items" :key="i.title" :to="i.url" link>
+            <v-list-item-title>{{i.title}}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-app-bar color="orange darken-1" dark app clipped-left>
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title class="v-list-item--link">
+          <span style="font-weight:bold" @click="$router.push('/'); drawer = false">人狼ゲームツール集</span>
+        </v-toolbar-title>
+      </v-app-bar>
+    </div>
+    <v-content>
+      <v-container fluid>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+<script lang="ts" src="@/assets/ts/main.ts" />
