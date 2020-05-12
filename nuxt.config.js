3e104ea6debd1154b24ed5e10856fe0b6626c49d
awataki <awataki1206@gmail.com>
2020-05-12 23:54:36 +0900

    fix title and discription

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,12 +6,11 @@ export default {
   ** Headers of the page
   */
   head: {
-    titleTemplate: "人狼ツール",
-    title: process.env.npm_package_name || '',
+    title: "人狼ツール",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: "人狼ゲームツール群" }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/static/favicon.ico' }
